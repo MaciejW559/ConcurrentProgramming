d6ba9549ebbeaba2ceb7596cc3d6ba7a1c85bb79
254901 <254901@edu.p.lodz.pl>
2026-03-12 22:16:20 +0100

    create unit test projects with sample tests

---
+++ DataTest/Test1.cs
@@ -0,0 +1,16 @@
+﻿using Data;
+
+namespace DataTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Liczby numers = new Liczby(10, 12);
+            Assert.AreEqual(10, numers.getA());
+            Assert.AreEqual(12, numers.getB());
+        }
+    }
+}
---
+++ LogicTest/Test1.cs
@@ -0,0 +1,16 @@
+﻿using Data;
+using Logic;
+
+namespace LogicTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Obliczenia obliczenia = new Obliczenia(new Liczby(10, 12));
+            Assert.AreEqual(22, obliczenia.dodaj());
+        }
+    }
+}
